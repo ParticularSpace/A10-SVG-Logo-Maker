42b3154afb0a9f77c01add32ec90052368586463
Sam Jones <samejones2018@gmail.com>
2023-04-14 20:12:42 -0700

    finished up my color,logoMaker js files also tested the maker

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-//call my required modules
-const inquirer = require('inquirer');
+//import required modules
 const fs = require('fs');
-const {Triangle, Circle, Square} = require("./lib/shapes");
+// const {Triangle, Circle, Square} = require("./lib/shapes");
 const {isValidColor} = require("./lib/color");
-const LogoGenerator = require("./lib/logoGenerator");
+const LogoMaker = require("./lib/logoMaker");
 
+(async () => {
+
+  // Import inquirer
+ const { default: inquirer } = await import('inquirer'); 
 
 //Questions for the user to answer
 async function promptUser() {
   const questions = [
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape for your logo.",
+      choices: ["Triangle", "Circle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "What color do you want your shape to be?",
+      validate: (input) => {
+        if (isValidColor(input)) {
+          return true;
+        }
+        return "Please enter a valid color keyword or hexadecimal number.";
+      },
+    },
     {
       type: "input",
       name: "text",
@@ -30,47 +50,35 @@ async function promptUser() {
         }
         return "Please enter a valid color keyword or hexadecimal number.";
       },
-    },
-    {
-      type: "list",
-      name: "shape",
-      message: "Choose a shape for your logo.",
-      choices: ["Triangle", "Circle", "Square"],
-    },
-    {
-      type: "input",
-      name: "shapeColor",
-      message: "Enter the shape's color (color keyword or hexadecimal number):",
-      validate: (input) => {
-        if (isValidColor(input)) {
-          return true;
-        }
-        return "Please enter a valid color keyword or hexadecimal number.";
-      },
-    },
+    }
   ];
 
   const answers = await inquirer.prompt(questions);
   return answers;
 }
 
-//function to write the file
+//Calls user to input answers
+//Gathers all the answers up
+//Takes answers and pushes them into the LogoMaker class
+//Use LogoMaker class to generate the logo
+//Save the logo to a file
 async function main() {
     const answers = await promptUser();
-    const logoGenerator = new LogoGenerator(
+    const logoMaker = new LogoMaker(
       answers.text,
       answers.textColor,
       answers.shape,
       answers.shapeColor
     );
-    const svgMarkup = logoGenerator.generateLogo();
+    const svgMarkup = logoMaker.generateLogo();
     saveSVGToFile(svgMarkup);
     console.log("Generated logo.svg");
   }
 
+  //write the file that contains our svg logo
   function saveSVGToFile(svgMarkup) {
     fs.writeFileSync("logo.svg", svgMarkup, "utf8");
   }
   
   main();
-
+})();
